be43c560d7b80fe8f638f9c2271ad2fc99df4055
siakim.1994@gmail.com <siakim.1994@gmail.com>
2023-06-17 13:48:24 +0900

    Chore : 깃 테스트

--- ipyang/src/main/java/com/project/ipyang/domain/adopt/entity/Adopt.java
+++ ipyang/src/main/java/com/project/ipyang/domain/adopt/entity/Adopt.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder //확인용
 public class Adopt extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
